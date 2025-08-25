d0daf0c65d4471883766f74e37e2e3d8e9bb425c
itxmuhammadwaqarali <itxmewaqar@gmail.com>
2025-08-25 15:46:11 +0500

    added API view class

--- core/api/urls.py
+++ core/api/urls.py
@@ -1,9 +1,10 @@
-from home.views import index , person , login # Import the index view from home app
+from home.views import index , person , login , PersonView # Import the index view from home app
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('index/', index, name='index'),  # Add a URL pattern for the index view
-    path('person/', person),  # Admin URL
-    path('login/', login),  # Admin URL
+    path('person/', person),
+    path('login/', login),
+    path('persons/', PersonView.as_view()),  # URL for the PersonView class-based view
 ]
