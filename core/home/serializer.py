a740a7a8a3fd4e87f4a8df1ea726e72e9f879887
itxmuhammadwaqarali <itxmewaqar@gmail.com>
2025-08-25 12:04:49 +0500

    removing venv

--- core/home/serializer.py
+++ core/home/serializer.py
@@ -5,3 +5,8 @@ class PeopleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
         fields = '__all__'  # This will include all fields from the Person model
+
+    def validate_age(self, data):
+        if data['age'] < 18:
+            raise serializers.ValidationError("Age must be at least 18.")
+        return data
